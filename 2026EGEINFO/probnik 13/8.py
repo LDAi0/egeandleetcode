a59cc5efab2794136796915b6f01f2c81e5fb26c
LDAi0 <kuularzape@gmail.com>
2026-05-25 00:37:52 +0700

    12probFIX

--- 2026EGEINFO/probnik 13/8.py
+++ 2026EGEINFO/probnik 13/8.py
@@ -1,9 +1,9 @@
 from itertools import product
 
 n=1
-for x in product('агилмор',repeat=5):
+for x in product('агилморт',repeat=5):
     a=''.join(x)
-    if n%2==0 and (a[0]!='а' or a[0]!='г'):
+    if n%2==0 and a[0]!='а' and a[0]!='г':
         if a.count('р')>=2:
             print(n)
             break
